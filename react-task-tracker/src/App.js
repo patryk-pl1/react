1ac9d7cefd0bb6ecb5c1e475da3904936e1e64e9
patryk-pl1 <81013592+patryk-pl1@users.noreply.github.com>
2021-08-09 19:24:56 +0200

    aplikacja

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header title="hello"/>
+    </div>
+  );
+}
+
+export default App;
